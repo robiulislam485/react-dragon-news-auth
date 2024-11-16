5a97b1d9f810605a0d31cd558594aaafefb87e74
robiulislam485 <jibonmahmudrobi485@gmail.com>
2024-11-16 21:17:10 +0600

    first commit

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const AuthLayout = () => {
+    return (
+        <div>
+            authLayout...
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default AuthLayout;
